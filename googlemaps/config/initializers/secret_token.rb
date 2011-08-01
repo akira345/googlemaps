2e644bf37d01031043a0ba160d4b84e964ef4784
akira345 <dosmania@dosmania.dip.jp>
2011-08-01 20:54:19 +0900

    first commit

---
+++ googlemaps/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Googlemaps::Application.config.secret_token = '90dbcd60f0b5c3492f65bb0565c7c971fde6a12c40bcc88970f14381bec785b69f71612323b8cf3ca7e2209ff484912a573ffcaecd786067d8426a05457ef3d3'
